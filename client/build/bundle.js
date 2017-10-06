205c98e78612bd871e52d457be2667fb148523b8
Stuart Moir <stuart.a.moir@live.com>
2017-10-06 12:09:32 +0100

    Adds some basic looping (one row only; no end cells) to the rules check in play.js

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -9951,33 +9951,31 @@ const play = grid => {
 
   let newGrid = Array(100).fill(false);
 
-  let i = 11;
+  for (let i = 11; i < 19; i++) {
 
-  let count = 0;
-  const cells = [-11, -10, -9, -1, 1, 9, 10, 11]; // Relative cells.
-  for (let j = 0; j < cells.length; j++) {
-    if (grid[i + cells[j]]) {
-      count++;
+    let count = 0;
+    const cells = [-11, -10, -9, -1, 1, 9, 10, 11]; // Relative cells.
+    for (let j = 0; j < cells.length; j++) {
+      if (grid[i + cells[j]]) {
+        count++;
+      }
     }
+
+    if (grid[i] && (count < 2 || count > 3)) {
+      newGrid[i] = false; // Live cell dies.
+      //console.log ('Live cell dies...', newGrid[i])
+    } else if (!grid[i] && count === 3) {
+      newGrid[i] = true; // Dead cell lives.
+      //console.log('Dead cell lives...', newGrid[i])
+    } else if (grid[i]) {
+      newGrid[i] = true; // Live cell lives.
+      //console.log('Live cell lives...', newGrid[i])
+    } else {
+      newGrid[i] = false; // Dead cell remains dead.
+      //console.log('Dead cell remains dead...', newGrid[i])
+    }
+    console.log(newGrid[i]);
   }
-
-  console.log(count);
-
-  if (grid[i] && (count < 2 || count > 3)) {
-    newGrid[i] = false; // Live cell dies.
-    console.log('Live cell dies...', newGrid[i]);
-  } else if (!grid[i] && count === 3) {
-    newGrid[i] = true; // Dead cell lives.
-    console.log('Dead cell lives...', newGrid[i]);
-  } else if (grid[i]) {
-    newGrid[i] = true; // Live cell lives.
-    console.log('Live cell lives...', newGrid[i]);
-  } else {
-    newGrid[i] = false; // Dead cell remains dead.
-    console.log('Dead cell remains dead...', newGrid[i]);
-  }
-
-  console.log(newGrid[i]);
 };
 /* harmony export (immutable) */ __webpack_exports__["a"] = play;
 
