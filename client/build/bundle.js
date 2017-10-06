2ad26f92089211c067a8a584d1113bf649e2dfb8
Stuart Moir <stuart.a.moir@live.com>
2017-10-06 13:52:05 +0100

    Adds play intermediate play functionality, tested for top two rows

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -9623,7 +9623,6 @@ module.exports = getIteratorFn;
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__components_cell__ = __webpack_require__(50);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__components_control__ = __webpack_require__(86);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__models_play_js__ = __webpack_require__(88);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__models_play_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4__models_play_js__);
 
 
 
@@ -9653,7 +9652,7 @@ class Game extends __WEBPACK_IMPORTED_MODULE_0_react___default.a.Component {
     label === 'Start' ? label = 'Stop' : label = 'Start';
     this.setState({ btnLabel: label });
     // play(this.state.squares)
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_4__models_play_js__["play"])(this.state.squares);
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_4__models_play_js__["a" /* play */])(this.state.squares);
   }
 
   handleClickResetBtn() {
@@ -9942,10 +9941,91 @@ class Grid extends __WEBPACK_IMPORTED_MODULE_0_react___default.a.Component {
 
 /***/ }),
 /* 88 */
-/***/ (function(module, __webpack_exports__) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-throw new Error("Module build failed: SyntaxError: Unexpected token (18:42)\n\n\u001b[0m \u001b[90m 16 | \u001b[39m      \u001b[36mconst\u001b[39m cells \u001b[33m=\u001b[39m [\u001b[35m1\u001b[39m\u001b[33m,\u001b[39m \u001b[35m10\u001b[39m\u001b[33m,\u001b[39m \u001b[35m11\u001b[39m]\n \u001b[90m 17 | \u001b[39m      console\u001b[33m.\u001b[39mlog(\u001b[32m'Top-left corner cell...'\u001b[39m)\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 18 | \u001b[39m    } \u001b[36melse\u001b[39m \u001b[36mif\u001b[39m (i \u001b[33m>\u001b[39m \u001b[35m0\u001b[39m \u001b[33m&&\u001b[39m \u001b[33m<\u001b[39m firstRowLastCell){ \u001b[90m// Top row cells.\u001b[39m\n \u001b[90m    | \u001b[39m                                          \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 19 | \u001b[39m      \u001b[36mconst\u001b[39m cells \u001b[33m=\u001b[39m [\u001b[33m-\u001b[39m\u001b[35m1\u001b[39m\u001b[33m,\u001b[39m \u001b[35m1\u001b[39m\u001b[33m,\u001b[39m \u001b[35m9\u001b[39m\u001b[33m,\u001b[39m \u001b[35m10\u001b[39m\u001b[33m,\u001b[39m \u001b[35m11\u001b[39m]\n \u001b[90m 20 | \u001b[39m      console\u001b[33m.\u001b[39mlog(\u001b[32m'Top row cells...'\u001b[39m)\n \u001b[90m 21 | \u001b[39m    } \u001b[36melse\u001b[39m \u001b[36mif\u001b[39m (i \u001b[33m===\u001b[39m rowLen){ \u001b[90m// Top-right corner cell.\u001b[39m\u001b[0m\n");
+
+const play = grid => {
+  //console.log('Play...')
+  //console.log(grid)
+
+  let newGrid = Array(100).fill(false);
+
+  for (let i = 0; i < 20; i++) {
+
+    console.log('CELL', i);
+
+    let rowLen = Math.sqrt(grid.length);
+    let firstRowLastCell = rowLen - 1;
+    let cells;
+
+    // Relative cells to check.
+    if (i === 0) {
+      // Top-left corner cell.
+      cells = [1, 10, 11];
+      console.log('Top-left corner cell...');
+    } else if (i > 0 && i < firstRowLastCell) {
+      // Top row cells/ not corners.
+      cells = [-1, 1, 9, 10, 11];
+      console.log('Top row cells/ not corners...');
+    } else if (i === firstRowLastCell) {
+      // Top-right corner cell.
+      cells = [-11, -10, -1, 9, 10];
+      console.log('Top-right corner cell...');
+    } else if (i % rowLen === 0) {
+      // Far-left column cells/ not corners
+      cells = [-10, -9, 1, 10, 11];
+      console.log('Far-left column cells/ not corners...');
+      // Far-right column cells/ not corners.
+    } else if (i % rowLen === firstRowLastCell) {
+      cells = [-11, -10, -1, 9, 10];
+      console.log('Far-right column cells/ not corners...');
+    } else if (i === grid.length - rowLen) {
+      // Botton-left corner cell.
+      cells = [-10, -9, 1];
+      console.log('Botton-left corner cell...');
+      // Bottom row cells/ not corners.
+    } else if (i > grid.length - rowLen && i < grid.length - 1) {
+      cells = [-11, -10, -9, -1, 1];
+      console.log('Bottom row cells...');
+    } else if (i === grid.length - 1) {
+      // Bottom-right corner cell.
+      cells = [-11, -10, -1];
+      console.log('Bottom-right corner cell...');
+    } else {
+      // All non-boundary cells.
+      cells = [-11, -10, -9, -1, 1, 9, 10, 11];
+      console.log('All non-boundary cells...');
+    }
+
+    console.log(cells);
+
+    let count = 0;
+    for (let j = 0; j < cells.length; j++) {
+      if (grid[i + cells[j]]) {
+        count++;
+      }
+    }
+    console.log(count);
+
+    if (grid[i] && (count < 2 || count > 3)) {
+      newGrid[i] = false; // Live cell dies.
+      console.log('Live cell dies...', newGrid[i]);
+    } else if (!grid[i] && count === 3) {
+      newGrid[i] = true; // Dead cell lives.
+      console.log('Dead cell lives...', newGrid[i]);
+    } else if (grid[i]) {
+      newGrid[i] = true; // Live cell remains alive.
+      console.log('Live cell remains alive...', newGrid[i]);
+    } else {
+      newGrid[i] = false; // Dead cell remains dead.
+      console.log('Dead cell remains dead...', newGrid[i]);
+    }
+    console.log(newGrid[i]);
+  }
+};
+/* harmony export (immutable) */ __webpack_exports__["a"] = play;
+
 
 /***/ }),
 /* 89 */
--- client/src/models/play.js
+++ client/src/models/play.js
@@ -5,60 +5,66 @@ export const play = (grid) => {
 
   let newGrid = Array(100).fill(false)
 
-  for (let i = 11; i < 13; i++){
+  for (let i = 0; i < 20; i++){
 
-    let count = 0
-    let rowLen = grid.length % 10
+    console.log('CELL', i)
+    
+    let rowLen = Math.sqrt(grid.length)
     let firstRowLastCell = rowLen - 1
+    let cells
 
-    // Relative cells.
+    // Relative cells to check.
     if (i === 0){ // Top-left corner cell.
-      const cells = [1, 10, 11]
+      cells = [1, 10, 11]
       console.log('Top-left corner cell...')
-    } else if (i > 0 && < firstRowLastCell){ // Top row cells.
-      const cells = [-1, 1, 9, 10, 11]
-      console.log('Top row cells...')
-    } else if (i === rowLen){ // Top-right corner cell.
-      const cells = [-11, -10, -1, 9, 10]      
+    } else if (i > 0 && i < firstRowLastCell){ // Top row cells/ not corners.
+      cells = [-1, 1, 9, 10, 11]
+      console.log('Top row cells/ not corners...')
+    } else if (i === firstRowLastCell){ // Top-right corner cell.
+      cells = [-11, -10, -1, 9, 10]      
       console.log('Top-right corner cell...')
-    } else if (i % rowLen === 0){ // Far-left column cells.
-      const cells = [-10, -9, 1, 10, 11]
-      console.log('Far-left column cells...')
-    } else if (i % rowLen === firstRowLastCell){ // Far-right column cells.
-      const cells = [-11, -10, -1, 9, 10]
-      console.log('Far-right column cells...')
-    } else if (i === (gird.length - rowLen){ // Botton-left corner cell.
-      const cells = [-10, -9, 1]
+    } else if (i % rowLen === 0){ // Far-left column cells/ not corners
+      cells = [-10, -9, 1, 10, 11]
+      console.log('Far-left column cells/ not corners...')
+    // Far-right column cells/ not corners.
+    } else if (i % rowLen === firstRowLastCell){
+      cells = [-11, -10, -1, 9, 10]
+      console.log('Far-right column cells/ not corners...')
+    } else if (i === (grid.length - rowLen)){ // Botton-left corner cell.
+      cells = [-10, -9, 1]
       console.log('Botton-left corner cell...')
-    // Bottom row cells.
-    } else if (i > (grid.length - rowLen) && < (grid.length - 1)){
-      const cells = [-11, -10, -9, -1, 1]
+      // Bottom row cells/ not corners.
+    } else if (i > (grid.length - rowLen) && i < (grid.length - 1)){
+      cells = [-11, -10, -9, -1, 1]
       console.log('Bottom row cells...')
     } else if (i === (grid.length - 1)){ // Bottom-right corner cell.
-      const cells = [-11, -10, -1]
+      cells = [-11, -10, -1]
       console.log('Bottom-right corner cell...')
     } else { // All non-boundary cells.
-      const cells = [-11, -10, -9, -1, 1, 9, 10, 11]
+      cells = [-11, -10, -9, -1, 1, 9, 10, 11]
       console.log('All non-boundary cells...')
     }
-    
 
+    console.log(cells)
+    
+    let count = 0
     for (let j = 0; j < cells.length; j++){
       if (grid[i + cells[j]]) { count++ }
     }
+    console.log(count)
 
     if (grid[i] && (count < 2 || count > 3)){
       newGrid[i] = false // Live cell dies.
-      //console.log ('Live cell dies...', newGrid[i])
+      console.log ('Live cell dies...', newGrid[i])
     } else if (!grid[i] && count === 3){
       newGrid[i] = true // Dead cell lives.
-      //console.log('Dead cell lives...', newGrid[i])
+      console.log('Dead cell lives...', newGrid[i])
     } else if (grid[i]){
-      newGrid[i] = true // Live cell lives.
-      //console.log('Live cell lives...', newGrid[i])
+      newGrid[i] = true // Live cell remains alive.
+      console.log('Live cell remains alive...', newGrid[i])
     } else {
       newGrid[i] = false // Dead cell remains dead.
-      //console.log('Dead cell remains dead...', newGrid[i])
+      console.log('Dead cell remains dead...', newGrid[i])
     }
     console.log(newGrid[i])
   }
