c9c7061dc2b324c68a524ae856f6e8db11a76a5a
Stuart Moir <stuart.a.moir@live.com>
2017-10-10 11:39:52 +0100

    Adds completed grid size selection menu for 10x10 and 20x20 grids; adds some changes to support 20x40 grid

---
+++ client/src/models/grid_sizes.js
@@ -0,0 +1,18 @@
+
+export const gridSizes = () => {
+  
+  return [
+    {
+      "name": "10 x 10",
+      "rowLen": 10
+    },
+    {
+      "name": "20 x 20",
+      "rowLen": 20
+    },
+    {
+      "name": "20 x 40",
+      "rowLen": 40
+    }
+  ]
+}
